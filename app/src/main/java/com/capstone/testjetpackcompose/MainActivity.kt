ac867e39b7425bc69c5fa4fd4cc3cc1c2a27e864
Airi <airiagustus82@gmail.com>
2024-06-24 03:09:05 +0700

    Add test list using loop for

--- app/src/main/java/com/capstone/testjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/capstone/testjetpackcompose/MainActivity.kt
@@ -7,18 +7,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.capstone.testjetpackcompose.ui.theme.TestJetpackComposeTheme
+
+private var sampleName = listOf(
+    "Andre",
+    "Budi",
+    "Caca",
+    "Dedi",
+    "Eko",
+    "Fifi",
+    "Gaga",
+    "Hedi",
+    "Ika",
+    "Joko",
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,42 +43,61 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TestJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TestJetpackComposeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Row {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Logo",
-        )
+fun TestJetpackComposeApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        GreetingList(sampleName)
+    }
+}
 
-        Column {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Text(text = "Welcome Ges!")
-        }
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Composable
+fun TestJetpackComposeAppPreview() {
+    TestJetpackComposeTheme {
+        TestJetpackComposeApp()
     }
 }
 
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TestJetpackComposeTheme {
-        Greeting("Android")
+fun GreetingList(names: List<String>) {
+    if (names.isNotEmpty()) {
+        Column {
+            for (name in names) {
+                Greeting(name)
+            }
+        }
+    } else {
+        Text(text = "No people to great :(")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.jetpack_compose),
+            contentDescription = "Logo Jetpack Compose",
+            modifier = Modifier.size(80.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(
+                text = "Hello $name",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+            )
+            Text(text = "Welcome to jetpack compose")
+        }
     }
 }
