892b1135fcae68d9b4bf0bc3e50b921f73bccfb4
Airi <airiagustus82@gmail.com>
2024-06-27 04:20:02 +0700

    Add animation and button for more and less function

--- app/src/main/java/com/capstone/testjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/capstone/testjetpackcompose/MainActivity.kt
@@ -4,17 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -71,23 +88,37 @@ fun TestJetpackComposeAppPreview() {
 @Composable
 fun GreetingList(names: List<String>) {
     if (names.isNotEmpty()) {
-        Column {
-            for (name in names) {
-                Greeting(name)
+        LazyColumn {
+            items(names) { name ->
+                Greeting(name = name)
             }
         }
     } else {
-        Text(text = "No people to great :(")
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "No people to greet :(")
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Row {
+    var isExpanded by remember { mutableStateOf(false) }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ), label = "Bouncing icon"
+    )
+
+    Row(
+        modifier = Modifier.padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Image(
             painter = painterResource(id = R.drawable.jetpack_compose),
             contentDescription = "Logo Jetpack Compose",
-            modifier = Modifier.size(80.dp)
+            modifier = Modifier.size(animatedSizeDp)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column {
@@ -95,9 +126,14 @@ fun Greeting(name: String) {
                 text = "Hello $name",
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
-                maxLines = 1,
             )
             Text(text = "Welcome to jetpack compose")
+        }
+        IconButton(onClick = { isExpanded = !isExpanded }) {
+            Icon(
+                imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (isExpanded) "Show less" else "Show more"
+            )
         }
     }
 }
